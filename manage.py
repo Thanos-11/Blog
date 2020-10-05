3787f87765f5070d1a2dd567db0a8feb6eba4048
Ted Mwangi <tedletheorde@gmail.com>
2020-10-05 11:27:30 +0300

    imported the models

--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@ from app import create_app
 from flask_script import Manager,Server
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
-from app.models import User
+from app.models import User,Quotes,Comment,Subscription
+
+
 
 
 
